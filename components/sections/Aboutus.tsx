463228b4b751e9806274b0d956f462d76af41b38
mwjawed@gmail.com <mwjawed@gmail.com>
2026-09-18 01:48:01 +0500

    About page: let side images bleed over the card edges like the original design

--- components/sections/Aboutus.tsx
+++ components/sections/Aboutus.tsx
@@ -39,10 +39,10 @@ const Aboutus = () => {
     <div>
       <section className="py-16">
         <div
-          className="container mx-auto px-4 sm:px-8 rounded-lg overflow-hidden shadow-lg bg-white p-6 sm:p-8 w-full sm:w-10/12 flex lg:flex-row flex-col-reverse items-center"
+          className="container mx-auto px-4 sm:px-8 rounded-lg shadow-lg bg-white p-6 sm:p-8 w-full sm:w-10/12 flex lg:flex-row flex-col-reverse items-center"
           style={{ backgroundImage: `url(${bg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-          <div className="w-full lg:w-1/3 mt-4 lg:mt-0">
+          <div className="w-full lg:w-1/3 mt-4 lg:mt-0 lg:-ml-24 xl:-ml-32">
             <Image
               src={abp1}
               alt="AboutPage"
@@ -69,7 +69,7 @@ const Aboutus = () => {
               ))}
             </div>
           </div>
-          <div className="w-full lg:w-1/3 mt-4 lg:mb-0">
+          <div className="w-full lg:w-1/3 mt-4 lg:mb-0 lg:-mr-24 xl:-mr-32">
             <Image
               src={abp2}
               alt="AboutPage"
